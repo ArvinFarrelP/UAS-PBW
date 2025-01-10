f91e19389b993c35a467ab6b84ceecefbe08a4d7
wahyudwiw <wdwi40996@gmail.com>
2025-01-10 23:22:35 +0700

    menambahkan inputdata buku

--- homepage.php
+++ homepage.php
@@ -18,19 +18,12 @@ include "koneksi.php";
     <!-- CDN BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- CDN JQUERY -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-    <!-- CSS -->
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
 <body>
     <header class="bg-dark text-white py-3">
         <div class="container d-flex justify-content-between align-items-center">
             <nav class="d-flex gap-3">
                 <a href="homepage.php" class="text-white text-decoration-none">Beranda</a>
-                <a href="inputbuku.php" class="text-white text-decoration-none">Input Buku</a>
+                <a href="input.php" class="text-white text-decoration-none">Input Buku</a>
                 <a href="inputpenulis.php" class="text-white text-decoration-none">Input Penulis</a>
                 <a href="datapenulis.php" class="text-white text-decoration-none">Data Penulis</a>
             </nav>
@@ -39,7 +32,10 @@ include "koneksi.php";
             </div>
         </div>
     </header>
+    <?php
+        $dbh = $koneksi->query("SELECT id, judul, tahun, nama_penulis FROM buku NATURAL JOIN penulis WHERE buku.isdel = 0");
 
+        ?>
     <div class="container mt-5">
         <div class="row">
             <table class="table table-bordered" border="1">
@@ -54,31 +50,23 @@ include "koneksi.php";
                 </thead>
                 <tbody>
                     <?php
-                    try {
-                        $query = $koneksi->query("
-                            SELECT buku.id AS id, buku.judul AS judul, buku.tahun AS tahun, penulis.nama_penulis AS penulis 
-                            FROM buku 
-                            INNER JOIN penulis ON buku.id_penulis = penulis.id_penulis 
-                            WHERE buku.isdel = 0
-                        ");
-                    } catch (PDOException $e) {
-                        die("Terjadi kesalahan: " . $e->getMessage());
-                    }
-
                     $no = 1;
-                    while ($bukus = $query->fetch(PDO::FETCH_ASSOC)) {
+                    while ($bukus = $dbh->fetch(PDO::FETCH_ASSOC)) {
                     ?>
-                    <tr>
-                        <th scope="row"><?php echo $no ?></th>
-                        <td><?php echo htmlspecialchars($bukus['judul']) ?></td>
-                        <td><?php echo htmlspecialchars($bukus['tahun']) ?></td>
-                        <td><?php echo htmlspecialchars($bukus['penulis']) ?></td>
-                        <td>
-                            <a class="btn btn-primary" href="edit.php?id=<?php echo $bukus['id'] ?>">Edit</a>
-                            <a class="btn btn-danger" href="delete.php?id=<?php echo $bukus['id'] ?>">Hapus</a>
-                        </td>
-                    </tr>
+                        <tr>
+                            <th scope="row"><?php echo $no ?></th>
+                            <td><?php echo $bukus['judul'] ?></td>
+                            <td><?php echo $bukus['tahun'] ?></td>
+                            <td><?php echo $bukus['nama_penulis'] ?></td>
+                            <td>
+                                <a class="btn btn-primary" href="edit.php?id=<?php echo $bukus['id'] ?>">Edit</a>
+                                <a class="btn btn-danger" href="delete.php?id=<?php echo $bukus['id'] ?>">Hapus</a>
+
+                            </td>
+                        </tr>
+
                     <?php
+
                         $no++;
                     }
                     ?>
