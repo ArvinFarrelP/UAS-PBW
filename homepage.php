2a8739260c7d17b734d42a8bb38d587baa465fbc
Arvin Farrel Pramuditya <farel877629@gmail.com>
2025-01-11 08:30:33 +0800

    Ubah warna navbar dan tombol logout di homepage.php

--- homepage.php
+++ homepage.php
@@ -17,9 +17,11 @@ include "koneksi.php";
 
     <!-- CDN BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
 
 <body>
-    <header class="bg-dark text-white py-3">
+    <header class="bg-primary text-white py-3">
+        <!-- Navbar warna biru -->
         <div class="container d-flex justify-content-between align-items-center">
             <nav class="d-flex gap-3">
                 <a href="homepage.php" class="text-white text-decoration-none">Beranda</a>
@@ -28,14 +30,15 @@ include "koneksi.php";
                 <a href="datapenulis.php" class="text-white text-decoration-none">Data Penulis</a>
             </nav>
             <div class="d-flex">
-                <a href="logout.php" class="btn btn-light">Logout</a>
+                <a href="logout.php" class="btn btn-danger">Logout</a> <!-- Tombol logout warna merah -->
             </div>
         </div>
     </header>
-    <?php
-        $dbh = $koneksi->query("SELECT id, judul, tahun, nama_penulis FROM buku NATURAL JOIN penulis WHERE buku.isdel = 0");
 
-        ?>
+    <?php
+    $dbh = $koneksi->query("SELECT id, judul, tahun, nama_penulis FROM buku NATURAL JOIN penulis WHERE buku.isdel = 0");
+    ?>
+
     <div class="container mt-5">
         <div class="row">
             <table class="table table-bordered" border="1">
@@ -53,20 +56,18 @@ include "koneksi.php";
                     $no = 1;
                     while ($bukus = $dbh->fetch(PDO::FETCH_ASSOC)) {
                     ?>
-                        <tr>
-                            <th scope="row"><?php echo $no ?></th>
-                            <td><?php echo $bukus['judul'] ?></td>
-                            <td><?php echo $bukus['tahun'] ?></td>
-                            <td><?php echo $bukus['nama_penulis'] ?></td>
-                            <td>
-                                <a class="btn btn-primary" href="edit.php?id=<?php echo $bukus['id'] ?>">Edit</a>
-                                <a class="btn btn-danger" href="delete.php?id=<?php echo $bukus['id'] ?>">Hapus</a>
-
-                            </td>
-                        </tr>
+                    <tr>
+                        <th scope="row"><?php echo $no ?></th>
+                        <td><?php echo $bukus['judul'] ?></td>
+                        <td><?php echo $bukus['tahun'] ?></td>
+                        <td><?php echo $bukus['nama_penulis'] ?></td>
+                        <td>
+                            <a class="btn btn-primary" href="edit.php?id=<?php echo $bukus['id'] ?>">Edit</a>
+                            <a class="btn btn-danger" href="delete.php?id=<?php echo $bukus['id'] ?>">Hapus</a>
+                        </td>
+                    </tr>
 
                     <?php
-
                         $no++;
                     }
                     ?>
