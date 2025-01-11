cb1ed8207c9afdeba6f8f165a784d6e9e2694063
Arvin Farrel Pramuditya <farel877629@gmail.com>
2025-01-11 08:53:49 +0800

    menambahkan sedikit css di hommepage.php

--- homepage.php
+++ homepage.php
@@ -17,6 +17,9 @@ include "koneksi.php";
 
     <!-- CDN BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
